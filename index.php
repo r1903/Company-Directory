3947f77829e030923efe8f6b70305a3dd54336f8
r1903 <55586316+r1903@users.noreply.github.com>
2020-09-18 10:24:39 +0100

    changed model heading

--- index.php
+++ index.php
@@ -183,7 +183,7 @@
                     <div class="modal-dialog">
                         <div class="modal-content">
                             <div class="modal-header">
-                                <h4 class="modal-title">Modal Heading</h4>
+                                <h4 class="modal-title">Delete confirmation</h4>
                                 <button type="button" class="close" data-dismiss="modal">&times;</button>
                             </div>
                             <div class="modal-body" id="deletModelBody">
